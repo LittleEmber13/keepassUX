c05cf47043d5f1bbfde1c89092f28012693e93e7
Daniel <102373314+danielduran-magnet@users.noreply.github.com>
2025-11-10 18:00:09 +0100

    feat: start ui

--- lib/ui/pages/start_page.dart
+++ lib/ui/pages/start_page.dart
@@ -15,6 +15,18 @@ class StartPage extends StatefulWidget {
 }
 
 class _StartPageState extends State<StartPage> {
+  TextEditingController passwordController = TextEditingController();
+  TextEditingController folderController = TextEditingController();
+
+  bool obscurePassword = false;
+
+  @override
+  void dispose() {
+    folderController.dispose();
+    passwordController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return BlocConsumer<KeePassBloc, KeePassState>(
@@ -39,9 +51,15 @@ class _StartPageState extends State<StartPage> {
   Widget _page() {
     return Scaffold(
       body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
+          SizedBox(),
+          Center(child: Text("KeePass", style: TextStyle(fontSize: 32))),
+          Padding(
+            padding: const EdgeInsets.all(24),
+            child: Container(color: Colors.black, height: 200),
+          ),
           Padding(
             padding: const EdgeInsets.symmetric(horizontal: 24),
             child: Container(
@@ -62,20 +80,124 @@ class _StartPageState extends State<StartPage> {
                 child: Column(
                   mainAxisSize: MainAxisSize.min,
                   children: [
-                    InkWell(
-                      onTap: () async {
-                        String password = 'test123';
-                        ByteData data = await rootBundle.load(
-                          'assets/test.kdbx',
-                        );
-                        Uint8List bytes = data.buffer.asUint8List();
-                        context.read<KeePassBloc>().add(
-                          LoadDatabase(bytes: bytes, password: password),
-                        );
-                      },
-                      child: Text("Select database"),
+                    Row(
+                      children: [
+                        Icon(Icons.folder_open),
+                        SizedBox(width: 16),
+                        Expanded(
+                          child: TextField(
+                            controller: folderController,
+                            decoration: InputDecoration(
+                              filled: true,
+                              fillColor: Color(0xFFF3F5F9),
+                              labelText: "Base de datos",
+                              enabledBorder: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(8),
+                                borderSide: BorderSide(
+                                  color: Color(0xFFD2D2D2),
+                                  width: 1,
+                                ),
+                              ),
+                              focusedBorder: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(8),
+                                borderSide: BorderSide(
+                                  color: Color(0xFFD2D2D2),
+                                  width: 1,
+                                ),
+                              ),
+                              contentPadding: const EdgeInsets.symmetric(
+                                horizontal: 12,
+                                vertical: 10,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                    SizedBox(height: 16),
+                    TextField(
+                      controller: passwordController,
+                      obscureText: obscurePassword,
+                      decoration: InputDecoration(
+                        filled: true,
+                        fillColor: Color(0xFFF3F5F9),
+                        labelText: "Contraseña",
+                        suffixIcon: IconButton(
+                          icon: Icon(
+                            obscurePassword == true
+                                ? Icons.visibility_outlined
+                                : Icons.visibility_off_outlined,
+                          ),
+                          onPressed: () {
+                            setState(() {
+                              obscurePassword = !obscurePassword;
+                            });
+                          },
+                        ),
+                        enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(8),
+                          borderSide: BorderSide(
+                            color: Color(0xFFD2D2D2),
+                            width: 1,
+                          ),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(8),
+                          borderSide: BorderSide(
+                            color: Color(0xFFD2D2D2),
+                            width: 1,
+                          ),
+                        ),
+                        contentPadding: const EdgeInsets.symmetric(
+                          horizontal: 12,
+                          vertical: 10,
+                        ),
+                      ),
                     ),
                   ],
+                ),
+              ),
+            ),
+          ),
+          Center(
+            child: Container(
+              decoration: BoxDecoration(
+                color: Color(0xFF374151),
+                borderRadius: BorderRadius.circular(8),
+              ),
+              child: InkWell(
+                onTap: () async {
+                  String password = 'test123';
+                  ByteData data = await rootBundle.load('assets/test.kdbx');
+                  Uint8List bytes = data.buffer.asUint8List();
+                  context.read<KeePassBloc>().add(
+                    LoadDatabase(bytes: bytes, password: password),
+                  );
+                },
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Container(
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(8),
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Icon(Icons.arrow_forward),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text(
+                          "Open database",
+                          style: TextStyle(color: Colors.white, fontSize: 18),
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
               ),
             ),
